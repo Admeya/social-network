26751ed932a57137b9377f5326c889fb7a4ff193
Bykova Irina <bykovaie@sovcombank.ru>
2020-10-12 23:19:08 +0300

    add liquibase

---
+++ src/main/java/com/highload/socialnetwork/configuration/PersistenceConfiguration.java
@@ -0,0 +1,14 @@
+package com.highload.socialnetwork.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PersistenceConfiguration {
+
+//    @Bean
+//    @Primary
+//    @ConfigurationProperties(prefix="spring.datasource")
+//    public DataSource primaryDataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
+}
